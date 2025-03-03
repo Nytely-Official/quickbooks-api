007fbba998bc355a185e8f0d69219ff37089e8a3
Jaiden <53905938+MimiPerson@users.noreply.github.com>
2025-03-03 23:37:52 +0200

    Update bill-query-builder.ts

--- src/packages/api/bill/bill-query-builder.ts
+++ src/packages/api/bill/bill-query-builder.ts
@@ -6,12 +6,4 @@ import { BaseQueryBuilder } from '../common/base-query-builder';
  * The Bill Query Builder
  */
 export class BillQueryBuilder extends BaseQueryBuilder<Bill> {
-	/**
-	 * Constructor
-	 * @param endpoint - The Endpoint
-	 * @param baseQuery - The Base Query
-	 */
-	constructor(endpoint: string, baseQuery: Query) {
-		super(endpoint, baseQuery);
-	}
 }
